8f3ad1c9945dfc621ab315383066cef0f8906c2f
anshu018 <anshu.official019@gmail.com>
2026-04-25 03:19:14 +0530

    chore: add .gitignore, remove build outputs from tracking

--- guardian-shield/parent-app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_guardianshield_parent_GuardianParentApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.guardianshield.parent.GuardianParentApp"
-)
-public final class _com_guardianshield_parent_GuardianParentApp {
-}
